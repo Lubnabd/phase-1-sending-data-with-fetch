0bd1edbc6050000ce7d99ff22b95a532a9c425b1
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-12-23 17:35:51 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData (userName, userEmail){;
+
+const userData = {
+    name: userName,
+    email: userEmail
+  };
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+  };
+
+  fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(data => {document.body.innerHTML = data.id}
+  )
+  .catch(function (error) {
+    document.body.innerHTML = err.message
+  });
+}
